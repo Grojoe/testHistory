7a75ea20f5d04bd61f5bf08ae55ec0e2ddaeb016
Nicolas <nicolasam@live.fr>
2019-12-16 14:43:15 +0100

    Test

---
+++ testHistory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace testHistory
+{
+    class Program
+    {
+
+        string password = "***REMOVED***";
+        static void Main(string[] args)
+        {
+            var a = 1;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ testHistory/Test/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testHistory.Test
+{
+    public class Contact
+    {
+        public int age { get; set; }
+        public string name { get; set; }
+
+        public Contact()
+        {
+            var a = "***REMOVED***";
+        }
+    }
+}
